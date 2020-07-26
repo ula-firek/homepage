e806c7e5466c53480dd76fec4bcf065396621d5d
Ula Firek <ula.firek@gmail.com>
2020-07-26 19:38:20 +0200

    let changed to const and created welcome and toggleBackground functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-console.log("Witajcie dobrzy ludzie. Fani BSB :)")
-
-let changeColorButton = document.querySelector(".button--changeBackground");
-let container = document.querySelector(".container");
-
-changeColorButton.addEventListener("click", () => {
-    container.classList.toggle("orangeBackground");
-
-});
-
-changeColorButton.addEventListener("click", () => {
-    if (changeColorButton.innerText === "Włącz pomarańczowy kolor tła") {
-        changeColorButton.innerText = "Wyłącz pomarańczowy kolor tła";
-    } else if (changeColorButton.innerText === "Wyłącz pomarańczowy kolor tła") {
-        changeColorButton.innerText = "Włącz pomarańczowy kolor tła";
+{
+    const welcome = () => {
+        console.log("Witajcie dobrzy ludzie. Fani BSB :)");
     }
-});
+
+    const toggleBackground = () => {
+        const container = document.querySelector(".js-body");
+        const changeColorButton = document.querySelector(".js-changeBackroundButton");
+
+        container.classList.toggle("body--orange");
+        if (changeColorButton.innerText === "Włącz pomarańczowy kolor tła") {
+            changeColorButton.innerText = "Wyłącz pomarańczowy kolor tła";
+        } else if (changeColorButton.innerText === "Wyłącz pomarańczowy kolor tła") {
+            changeColorButton.innerText = "Włącz pomarańczowy kolor tła";
+        }
+
+        changeColorButton.addEventListener("click", toggleBackground);
+    }
+
+    welcome();
+    toggleBackground()
+};
+
+
+
